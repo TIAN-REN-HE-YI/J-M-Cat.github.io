724bfbdce51dda7ca813a5332be300d89a1a5543
mao <mao_shangyong@163.com>
2023-01-02 19:40:09 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:TIAN-REN-HE-YI/J-M-Cat.github.io.git master:gh-pages
+git push -f git@github.com:TIAN-REN-HE-YI/J-M-Cat.github.io.git main:gh-pages
 
 cd -
